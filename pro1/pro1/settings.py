e8860758262f378d200061d297a1391328394fdc
Harishadithyan <harishadithyan17@gmail.com>
2024-12-03 10:02:30 +0530

    pull

--- pro1/pro1/settings.py
+++ pro1/pro1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3rrwx&r-@&*q-dngg@n@gpaljyxlguynw@1--g^@_h^d*@89qn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
